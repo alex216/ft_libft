3afe8bf345b870a2472d0784d7b9efcd35226437
alex216 <yyliu0907@gmail.com>
2023-09-28 22:40:20 +0900

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 21:56:54 by yliu              #+#    #+#             */
-/*   Updated: 2023/09/27 21:56:58 by yliu             ###   ########.fr       */
+/*   Updated: 2023/09/28 22:39:58 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,25 @@ t_list	*ft_lstmap(t_list *p, void *(*f)(void *), void (*del)(void *))
 	}
 	return (first);
 }
+
+int	main(void)
+{
+	char	*elem1 = "Hello";
+	char	*elem2 = "world";
+	char	*elem3 = "student";
+	int		elem4 = 42;
+	char	*elem5 = "!!";
+
+	t_list	*t1;
+	t_list	*t2;
+	t_list	*t3;
+	t_list	*t4;
+	t_list	*t5;
+
+	t1->next = t2;
+	t2->next = t3;
+	t3->next = t4;
+	t4->next = t5;
+
+}
+
