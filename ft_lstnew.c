85369e290ee79105557ab62b6e83c58e1ff7f713
yliu <yliu@student.42tokyo.jp>
2023-10-11 20:11:24 +0900

    fix lstnew SEGV

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 10:57:56 by yliu              #+#    #+#             */
-/*   Updated: 2023/09/30 01:18:56 by yliu             ###   ########.fr       */
+/*   Updated: 2023/10/11 20:11:04 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*lst_p;
 
+	if (content == NULL)
+		return (NULL);
 	lst_p = malloc(sizeof(t_list));
 	if (lst_p == NULL)
 		return (NULL);
