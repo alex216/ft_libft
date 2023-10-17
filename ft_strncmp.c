9cc21204a373ea6a46e61a9b6e7dcb2ba0f6e24d
yliu <yliu@XLIJFELKNIX.local>
2023-10-17 11:51:45 +0900

    [style] change order inside while condition

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,15 +14,15 @@
 
 // unsafe when either s1 or s2 is NULL.
 // but this is how original strncmp works.
+// putting i < n ahead inside while condition
+// result in this wired behaviour.
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if ((s1 == NULL && n == 0) || (s2 == NULL && n == 0))
-		return (0);
 	i = 0;
-	while (s1[i] != '\0' && s1[i] == s2[i] && i < n)
+	while (i < n && s1[i] != '\0' && s1[i] == s2[i])
 		i++;
 	if (i == n)
 		return (0);
