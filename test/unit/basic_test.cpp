3ce3458ca4651aa29417a44690fcf3eded686349
yliu <yliu@XLIJFELKNIX.local>
2024-04-21 14:53:13 +0900

    [feat] gtest for lstsize lstlast

--- test/unit/basic_test.cpp
+++ test/unit/basic_test.cpp
@@ -7,17 +7,39 @@ extern "C"
 #include "get_next_line.h"
 }
 
-TEST(ft_strlen, zero_size)
+TEST(ft_dl_lstsize, null)
 {
-	const char	*str = NULL;
-	EXPECT_EQ(ft_strlen(str), 0);
+	int ans = ft_dl_lstsize(NULL);
+	EXPECT_EQ(ans, 0);
 }
 
-// TEST(ft_dl_lstlast, double_linked_list_lstsize)
-// {
-// 	const char *envp[] = {"PATH=/bin:/usr/bin", NULL};
-// 	const char *cmd = "/bin/cmd42";
-// 	const char *err_msg = "bash: /bin/cmd42: No such file or directory";
-//
-// 	EXPECT_EXIT(return_entire_path(cmd, envp), ::testing::ExitedWithCode(127), err_msg);
-// }
+TEST(ft_dl_lstsize, size_2)
+{
+	t_lst	**lst_pp = (t_lst **)malloc(sizeof(t_lst **));
+	t_lst	*node1 = (t_lst *)malloc(sizeof(t_lst));
+	t_lst	*node2 = (t_lst *)malloc(sizeof(t_lst));
+	
+	ft_dl_lstadd_back_with_lst(lst_pp, node1);
+	ft_dl_lstadd_back_with_lst(lst_pp, node2);
+	EXPECT_EQ(ft_dl_lstsize(*lst_pp), 2);
+}
+
+TEST(ft_dl_lstlast, size_1)
+{
+	t_lst	**lst_pp = (t_lst **)malloc(sizeof(t_lst **));
+	t_lst	*node1 = (t_lst *)malloc(sizeof(t_lst));
+	
+	ft_dl_lstadd_back_with_lst(lst_pp, node1);
+	EXPECT_EQ(ft_dl_lstlast(*lst_pp), node1);
+}
+
+TEST(ft_dl_lstlast, size_2)
+{
+	t_lst	**lst_pp = (t_lst **)malloc(sizeof(t_lst **));
+	t_lst	*node1 = (t_lst *)malloc(sizeof(t_lst));
+	t_lst	*node2 = (t_lst *)malloc(sizeof(t_lst));
+	
+	ft_dl_lstadd_back_with_lst(lst_pp, node1);
+	ft_dl_lstadd_back_with_lst(lst_pp, node2);
+	EXPECT_EQ(ft_dl_lstlast(*lst_pp), node2);
+}
