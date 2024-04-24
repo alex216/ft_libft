2f443b8883d750cfe25edb695b9d1b5575bc7f4f
yliu <yliu@XLIJFELKNIX.local>
2024-04-24 10:38:34 +0900

    [feat] update dl_lstdelone

--- src/ft_dl_lst/ft_dl_lstdelone.c
+++ src/ft_dl_lst/ft_dl_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:26:37 by yliu              #+#    #+#             */
-/*   Updated: 2024/01/24 13:13:57 by yliu             ###   ########.fr       */
+/*   Updated: 2024/04/24 10:35:36 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@ void	ft_dl_lstdelone(t_lst *lst_p, void (*del)(void *))
 {
 	if (lst_p == NULL || del == NULL)
 		return ;
-	if (lst_p->is_sentinel == false)
-		del(lst_p);
+	if (lst_p->is_sentinel)
+	{
+		free(lst_p);
+		return ;
+	}
+	lst_p->prev_p->next_p = lst_p->next_p;
+	lst_p->next_p->prev_p = lst_p->prev_p;
+	del(lst_p);
 	free(lst_p);
 }
